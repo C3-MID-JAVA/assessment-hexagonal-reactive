3bb096e253b363f2ae516162208c22eeee681927
Mishell Yagual Mendoza <mishell.yagual@sofka.com.co>
2024-12-22 14:43:05 -0500

    Part 1 - Hexagonal Architecture

---
+++ domain/model/src/main/java/ec/com/sofka/Account.java
@@ -0,0 +1,41 @@
+package ec.com.sofka;
+
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String id;
+    private BigDecimal balance;
+    private String accountNumber;
+    private String owner;
+
+    public Account(BigDecimal balance, String owner, String accountNumber) {
+        this.balance = balance;
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
