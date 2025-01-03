c486768fb14237296b57f15dd81281d20a25867b
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2025-01-03 07:57:28 -0300

    Update project dependencies and package structure for FinancesPro application.

--- application/app-service/src/main/java/ec/com/sofka/FinancesProApplication.java
+++ application/app-service/src/main/java/ec/com/sofka/FinancesProApplication.java
@@ -1,19 +1,18 @@
-package org.example.financespro;
+package ec.com.sofka;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** Main class for the FinancesPro application. */
+/**
+ * Main application for FinancesPro.
+ */
 @SpringBootApplication
 public class FinancesProApplication {
+
   public static void main(String[] args) {
-    // Load .env file
     Dotenv dotenv = Dotenv.configure().directory("./").load();
-
-    // Set MongoDB connection environment variables
     System.setProperty("SPRING_MONGODB_URI", dotenv.get("SPRING_MONGODB_URI"));
-
     SpringApplication.run(FinancesProApplication.class, args);
   }
 }
