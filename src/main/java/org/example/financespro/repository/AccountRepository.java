4f2992363e24ab1391704379ab46acdf39f86442
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2025-01-03 07:56:19 -0300

    Remove deprecated classes and interfaces.

--- src/main/java/org/example/financespro/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.financespro.repository;
-
-import org.example.financespro.model.Account;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface AccountRepository extends ReactiveMongoRepository<Account, String> {
-  Mono<Account> findByAccountNumber(String accountNumber);
-}
--- src/main/java/org/example/financespro/repository/TransactionRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.financespro.repository;
-
-import org.example.financespro.model.Transaction;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface TransactionRepository extends ReactiveMongoRepository<Transaction, String> {}
